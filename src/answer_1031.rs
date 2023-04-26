97fb737aa8f37ab03ea2d29ae77e94eff668f47a
40629 <2406222717@qq.com>
2023-04-26 21:35:17 +0800

    代码化简

--- src/answer_1031.rs
+++ src/answer_1031.rs
@@ -1,3 +1,5 @@
+use std::cmp::max;
+
 // 纯爆破
 // 这两天看Rust圣经至少对泛形加深了不少理解
 pub fn sum<T:std::ops::Add<Output = T> + std::ops::Sub<Output=T> + Copy> (a:&[T]) -> T{
@@ -23,20 +25,17 @@ pub fn max_sum_two_no_overlap(nums: Vec<i32>, first_len: i32, second_len: i32) -
                 if max_ < sum_{max_ = sum_;}
                 index2 += 1;
             }
-
-            let first_ = sum(&nums[index..index + second_len]);
-            let mut index2 = index + second_len;
-            while (index2 < all_len - first_len + 1){
-                let sum_ =  first_ + sum(&nums[index2..index2 + first_len]);
-                if max_ < sum_{max_ = sum_;}
-                index2 += 1;
-            }
             index += 1;
         }
         max_
 }
 
+pub fn answer1031(nums: Vec<i32>, first_len: i32, second_len: i32) -> i32 {
+    let a = max_sum_two_no_overlap(nums.clone(), first_len.clone(), second_len.clone());
+    let b = max_sum_two_no_overlap(nums.clone(), second_len, first_len);
+    return max(a,b);
+}
 #[test]
 fn test(){
-    assert_eq!(max_sum_two_no_overlap(vec![4,5,14,16,16,20,7,13,8,15],3, 5), 109)
+    assert_eq!(answer1031(vec![4,5,14,16,16,20,7,13,8,15],3, 5), 109)
 }
